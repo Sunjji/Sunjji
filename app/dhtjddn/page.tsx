5eeaa70b9796119b77f5e1a87e0c8a0b0e03f1ed
오성우 <ad19eq@naver.com>
2024-10-10 17:21:44 +0900

    feat: supabase 설치,커넥트

--- app/dhtjddn/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Dhtjddn() {
-  return (
-    <div>Dhtjddn</div>
-  )
-}
-
-export default Dhtjddn
--- app/young/page.tsx
+++
@@ -1,8 +0,0 @@
-function YoungPage() {
-  return <main>page</main>;
-  <div>
-    ddsdasd
-  </div>
-}
-
-export default YoungPage;
