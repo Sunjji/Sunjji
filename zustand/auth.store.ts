9c8e75e5e5c36464c93b347b0f8d5f8d02eab765
오성우 <ad19eq@naver.com>
2024-10-14 15:10:47 +0900

    feat: profile and AuthProvider

--- zustand/auth.store.ts
+++ zustand/auth.store.ts
@@ -5,11 +5,15 @@ type AuthStoreState = {
   isLoggedIn: boolean;
   initializeAuth: () => void;
   setIsLoggedIn: (isLoggedIn: boolean) => void;
+  logIn: () => void;
+  logOut: () => void;
 };
 
 export const useAuthStore = create<AuthStoreState>((set) => ({
   isAuthInitialized: false,
   isLoggedIn: false,
+  logIn: () => set({ isLoggedIn: true }),
+  logOut: () => set({ isLoggedIn: false }),
   initializeAuth: () => set({ isAuthInitialized: true }),
   setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
 }));
