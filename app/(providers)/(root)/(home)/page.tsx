78a99b67f41a195f950dc4790ded8ca49fa032c5
오선주 <0728htw@naver.com>
2024-10-14 15:04:00 +0900

    feat: 달력 추가했습니둥

--- app/(providers)/(root)/(home)/page.tsx
+++ app/(providers)/(root)/(home)/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import dayGridPlugin from "@fullcalendar/daygrid";
+import FullCalendar from "@fullcalendar/react";
+
 function HomePage() {
   return (
     <>
-      <div className="w-[400px] h-[400px] bg-point"></div>
+      <div className="mt-[30px] ml-[50px] rounded-[8px] w-[600px] h-[400px] border bg-point">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          height="400px"
+          headerToolbar={{
+            start: "prev next",
+            center: "title",
+            end: "dayGridMonth dayGridWeek",
+          }}
+          locale={"ko"}
+          events={[
+            { title: "Event 1", date: "2024-06-01" },
+            { title: "Event 2", date: "2024-06-07" },
+          ]}
+        />
+      </div>
     </>
   );
 }
