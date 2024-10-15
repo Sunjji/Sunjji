770ec3b911e1917c7d41bc000d3a4af3f5411b46
jaeyup06 <jaeyup06@gmail.com>
2024-10-15 13:44:05 +0900

    debug: 달력 z-index 조정(달력이 모달보다 위에 있는 버그 수정)

--- app/(providers)/(root)/(home)/page.tsx
+++ app/(providers)/(root)/(home)/page.tsx
@@ -7,7 +7,7 @@ import FullCalendar from "@fullcalendar/react";
 function HomePage() {
   return (
     <>
-      <div className="mt-[30px] ml-[50px] rounded-[8px] w-[600px] h-[400px] border bg-point">
+      <div className="mt-[30px] ml-[50px] rounded-[8px] w-[600px] h-[400px] border bg-point absolute z-0">
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
@@ -25,6 +25,13 @@ function HomePage() {
           events={[
             { title: "Event 1", date: "2024-10-14" },
             { title: "Event 1", date: "2024-10-14" },
+            {
+              title: "고양이 밥 주기",
+              start: "2024-10-15",
+              end: "2024-10-17",
+              backgroundColor: "skyblue",
+              borderColor: "skyblue",
+            },
           ]}
         />
       </div>
