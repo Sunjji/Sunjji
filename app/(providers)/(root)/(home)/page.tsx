329042a0f9446a4ba5c3afdf8b565826e5dc2031
오선주 <0728htw@naver.com>
2024-10-16 16:29:19 +0900

    feat/달력기능 완료

--- app/(providers)/(root)/(home)/page.tsx
+++ app/(providers)/(root)/(home)/page.tsx
@@ -1,13 +1,46 @@
 "use client";
 
+import { supabase } from "@/supabase/client";
+import { useAuthStore } from "@/zustand/auth.store";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/react";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
 function HomePage() {
+  const [events, setEvents] = useState([]);
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    (async () => {
+      const { data } = await supabase.auth.getUser();
+      const authorId = data.user?.id;
+
+      const response = await supabase
+        .from("diaries")
+        .select()
+        .eq("authorId", authorId);
+
+      const diaries = response.data;
+
+      const events = diaries?.map((diary) => ({
+        title: diary.title,
+        date: dayjs(diary.createdAt).format("YYYY-MM-DD"),
+        url: `/diaries/${diary.id}/detail`,
+      }));
+
+      setEvents(events);
+
+      console.log("response", response);
+    })();
+  }, [isLoggedIn]);
+
   return (
     <>
-      <div className="mt-[30px] ml-[50px] rounded-[8px] w-[600px] h-[400px] border bg-point absolute z-0">
+      <div className="mt-[30px] ml-[50px] rounded-[8px] w-full  border bg-point absolute z-0">
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
@@ -22,10 +55,7 @@ function HomePage() {
           locale={"ko"}
           eventBackgroundColor="pink"
           eventBorderColor="pink"
-          events={[
-            { title: "Event 1", date: "2024-10-14" },
-            { title: "Event 1", date: "2024-10-14" },
-          ]}
+          events={events}
         />
       </div>
     </>
