5d70201cc21427195e6243c308716d74cece95dc
오선주 <0728htw@naver.com>
2024-10-22 11:21:15 +0900

    헤더 스타일 해씀용 로고도 추가되었슴듕

--- app/(providers)/(root)/_components/SideBar.tsx
+++ app/(providers)/(root)/_components/SideBar.tsx
@@ -28,52 +28,61 @@ function SideBar() {
 
   return (
     <>
-    <nav className="bg-point w-[70px] min-h-screen fixed z-10">
-      <Link href={"/"} className="font-bold text-2xl">
-        홈
-      </Link>
-      {isAuthInitialized ? ( // isAuthInitialized가 true일때 출력(삼항연산자)
-        <div>
-          {isLoggedIn ? ( // isLoggedIn이 true일때 출력
-            <>
-              <Link className="block text-sm mb-5" href={"/my-page"}>
-                마이페이지
-              </Link>
-              <Link className="block mb-5" href={"/diaries"}>
-                공개일기
-              </Link>
-              <Link className="block mb-5" href={"/views/userView"}>
-                내 일기
-              </Link>
-              <button onClick={handleClickLogOut}>로그아웃</button>
-            </>
-          ) : (
-            // isLoggedIn이 false일때 출력
-            <div>
-              <Link className="font-bold text-2xl mb-5" href={"/diaries"}>
-                공개일기
-              </Link>
-              <button
-                onClick={handleClickLogInButton}
-                className="font-bold text-2xl pb-3 pt-3"
-              >
-                로그인
-              </button>
-              <button>
-                <Link
-                  href={"/sign-up"}
+      <nav className="ml-5 w-[150px] min-h-screen fixed z-10">
+        <img
+          className="w-[250px] mb-5"
+          src="http://localhost:3000/assets/img/logo.png"
+          alt="logo"
+        />
+        <Link href={"/my-page"} className="font-bold text-2xl">
+          프로필(대충 해놓은거임)
+        </Link>
+        <br />
+        <Link href={"/"} className="block mt-5 mb-5 font-bold text-2xl">
+          홈
+        </Link>
+        {isAuthInitialized ? ( // isAuthInitialized가 true일때 출력(삼항연산자)
+          <div>
+            {isLoggedIn ? ( // isLoggedIn이 true일때 출력
+              <>
+                <Link className="block text-sm mb-5" href={"/my-page"}>
+                  마이페이지
+                </Link>
+                <Link className="block mb-5" href={"/diaries"}>
+                  공개일기
+                </Link>
+                <Link className="block mb-5" href={"/views/userView"}>
+                  내 일기
+                </Link>
+                <button onClick={handleClickLogOut}>로그아웃</button>
+              </>
+            ) : (
+              // isLoggedIn이 false일때 출력
+              <div>
+                <Link className="font-bold text-2xl mb-5" href={"/diaries"}>
+                  공개일기
+                </Link>
+                <button
+                  onClick={handleClickLogInButton}
                   className="font-bold text-2xl pb-3 pt-3"
                 >
-                  회원가입
-                </Link>
-              </button>
-            </div>
-          )}
-        </div>
-      ) : null}
-      {/* isAuthInitialized가 false일때 null을 출력 */}
-    </nav>
-    <div className="w-[75px]" />
+                  로그인
+                </button>
+                <button>
+                  <Link
+                    href={"/sign-up"}
+                    className="font-bold text-2xl pb-3 pt-3"
+                  >
+                    회원가입
+                  </Link>
+                </button>
+              </div>
+            )}
+          </div>
+        ) : null}
+        {/* isAuthInitialized가 false일때 null을 출력 */}
+      </nav>
+      <div className="w-[75px]" />
     </>
   );
 }
