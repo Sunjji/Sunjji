78ccf0ddc04e4c6bf4555d421b91bd22f483641e
shin <sinhaeun1234@gmail.com>
2024-10-23 11:35:30 +0900

    feat: commentsPageDevelop

--- app/(providers)/(root)/diaries/_components/CommentButton.tsx
+++ app/(providers)/(root)/diaries/_components/CommentButton.tsx
@@ -1,42 +1,15 @@
-"use client";
-
-import { supabase } from "@/supabase/client";
-import { ComponentProps, useEffect, useState } from "react";
 import { RiMessage3Line } from "react-icons/ri";
 
 interface CommentButtonProps {
   diaryId: string;
+  commentsCount: number;
 }
 
-function CommentButton({ diaryId }: CommentButtonProps) {
-  const [commentCount, setCommentCount] = useState<number>(0);
-
-  useEffect(() => {
-    // 댓글 수를 가져오는 함수
-    const fetchCommentCount = async () => {
-      const { count } = await supabase
-        .from("comments")
-        .select("id", { count: "exact" })
-        .eq("diaryId", diaryId);
-
-      setCommentCount(count || 0);
-    };
-
-    fetchCommentCount();
-  }, [diaryId]);
-
-  // 댓글 버튼 클릭
-  const handleCommentButtonClick: ComponentProps<"button">["onClick"] = () => {
-    // 댓글 버튼 눌렀을 때 기능
-  };
-
+function CommentButton({ commentsCount }: CommentButtonProps) {
   return (
-    <button
-      className="flex items-center z-20"
-      onClick={handleCommentButtonClick}
-    >
+    <button className="flex items-center z-20">
       <RiMessage3Line className="w-[30px] h-[30px] text-BrownPoint" />
-      <p>{commentCount}</p>
+      <p>{commentsCount}</p>
     </button>
   );
 }
--- app/(providers)/(root)/diaries/_components/Comments.tsx
+++ app/(providers)/(root)/diaries/_components/Comments.tsx
@@ -2,32 +2,52 @@
 
 import { supabase } from "@/supabase/client";
 import { Tables } from "@/supabase/database.types";
+import { useAuthStore } from "@/zustand/auth.store";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import CommentButton from "./CommentButton";
 import HeartButton from "./HeartButton";
 
+type CustomComment = Tables<"comments"> & { profile: Tables<"profiles"> };
+
 function Comments() {
   const params = useParams();
-  const [newContent, setNewContent] = useState("");
-  const [content, setContent] = useState<Tables<"comments">[]>([]);
   const { diaryId } = params;
+  const [newContent, setNewContent] = useState("");
+  const [comments, setComments] = useState<CustomComment[]>([]);
+
+  const currentUserId = useAuthStore((state) => state.currentUserId);
 
   useEffect(() => {
     (async () => {
-      // 댓글 정보 가져오기
-      const { data: response, error } = await supabase
+      // 댓글 가져오기
+      const { data: comments } = await supabase
         .from("comments")
         .select("*")
         .eq("diaryId", Number(diaryId));
 
-      if (error) {
-        return console.log("error", error);
-      } else {
-        setContent(response);
-      }
+      if (!comments) return console.log("comments error");
+
+      // 프로필 가져오기
+      const { data: profiles } = await supabase.from("profiles").select("*");
+
+      if (!profiles) return console.log("profiles error");
+
+      // 댓글 단 유저의 프로필 정보 찾기
+      const match = comments.map((comment) => {
+        const profile = profiles.find(
+          (profile) => comment.authorId === profile.id
+        );
+
+        return {
+          ...comment,
+          profile: profile,
+        };
+      });
+
+      setComments(match);
     })();
-  }, [content]);
+  }, []);
 
   const handleClickCommentButton = async () => {
     if (!newContent) return alert("댓글을 적어주세요");
@@ -37,37 +57,85 @@ function Comments() {
       .insert({ content: newContent, diaryId: Number(diaryId) });
 
     setNewContent(""); // textarea 빈칸으로 바꾸기
-    // }
+
+    refetchComments();
+  };
+
+  const refetchComments = async () => {
+    // supabase에서 댓글 다시 가져와서
+    const { data: comments } = await supabase
+      .from("comments")
+      .select("*")
+      .eq("diaryId", Number(diaryId))
+      .order("createdAt", { ascending: false });
+
+    // setComments에 다시 넣기
+    if (!comments) {
+      return console.log("error");
+    }
+    // 프로필 가져오기
+    const { data: profiles } = await supabase.from("profiles").select("*");
+
+    if (!profiles) return console.log("profiles error");
+
+    // 댓글 단 유저의 프로필 정보 찾기
+    const match = comments.map((comment) => {
+      const profile = profiles.find(
+        (profile) => comment.authorId === profile.id
+      );
+
+      return {
+        ...comment,
+        profile: profile,
+      };
+    });
+    setComments(match);
   };
 
   const handleClickDeleteButton = async (commentId: number) => {
-    const deleteComments = await supabase
-      .from("comments")
-      .delete()
-      .eq("id", commentId);
-
-    console.log(deleteComments);
+    await supabase.from("comments").delete().eq("id", commentId);
+    refetchComments();
   };
 
   return (
     <div className="flex flex-col w-[380px] bg-white rounded-lg shadow-xl">
       <div className="h-[550px] relative">
         <ul className="mt-2">
-          {content.map((comment) => (
-            <li className="flex px-2 py-1" key={comment.id}>
+          {comments.map((comment) => (
+            <li
+              key={comment.id}
+              className="flex items-center gap-x-4 px-2 py-1"
+            >
+              <img
+                className="w-14 h-14 rounded-full object-cover"
+                src={`${comment.profile?.imageUrl}`}
+                alt="프로필 이미지"
+              />
+
               <p
                 onClick={() => handleClickDeleteButton(comment.id)}
                 // 미완성
-                className="flex-grow cursor-pointer hover:line-through"
+                className={`flex-grow cursor-pointer ${
+                  comment.authorId === currentUserId
+                    ? "hover:line-through hover:text-red-600"
+                    : null
+                }
+                `}
               >
                 {comment.content}
               </p>
+
+              <p>{comment.profile?.nickname}</p>
             </li>
           ))}
         </ul>
         <div className="flex gap-2 absolute bottom-0 px-4 py-3 bg-white w-full z-5">
           <HeartButton diaryId={diaryId.toString()} />
-          <CommentButton diaryId={diaryId.toString()} />
+
+          <CommentButton
+            diaryId={diaryId.toString()}
+            commentsCount={comments.length}
+          />
         </div>
       </div>
       <div className="flex items-center z-10 shadow-[0_-4px_6px_rgba(0,0,0,0.05)]">
