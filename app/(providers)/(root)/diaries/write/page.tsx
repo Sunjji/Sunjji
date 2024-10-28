86edb3ac072261b2030494b6a7f43f0fcec0fbcf
jaeyup06 <jaeyup06@gmail.com>
2024-10-28 14:32:29 +0900

    style: 일기 작성 페이지 조정

--- app/(providers)/(root)/diaries/write/page.tsx
+++ app/(providers)/(root)/diaries/write/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { supabase } from "@/supabase/client";
@@ -143,7 +145,7 @@ function DiaryWritePage() {
         onSubmit={handleSubmitButton}
         className="flex flex-col bg-[#FEFBF2] rounded-[8px]"
       >
-        <div className="absolute top-[46px] ml-36">
+        <div className="absolute top-[88px] ml-40">
           <IsPublicToggle isPublic={isPublic} setIsPublic={setIsPublic} />
         </div>
 
