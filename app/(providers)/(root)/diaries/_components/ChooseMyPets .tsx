15024fdc4a90f2af080431ae41ee0cab8d9bd92d
shin <sinhaeun1234@gmail.com>
2024-10-28 11:34:48 +0900

    feat: writePage 수정

--- app/(providers)/(root)/diaries/_components/ChooseMyPets .tsx
+++ app/(providers)/(root)/diaries/_components/ChooseMyPets .tsx
@@ -3,10 +3,11 @@
 import api from "@/api/api";
 import Modal from "@/components/Modal";
 import { supabase } from "@/supabase/client";
+import { Tables } from "@/supabase/database.types";
 import { useAuthStore } from "@/zustand/auth.store";
 import { useModalStore } from "@/zustand/modal.store";
 import { useQuery } from "@tanstack/react-query";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 type ChooseMyPetsProps = {
   setSelectedPetIds: Dispatch<SetStateAction<number[]>>;
@@ -14,13 +15,30 @@ type ChooseMyPetsProps = {
 
 function ChooseMyPets({ setSelectedPetIds }: ChooseMyPetsProps) {
   const currentUserId = useAuthStore((state) => state.currentUserId);
+  const [profiles, setProfiles] = useState<Tables<"profiles">>();
+  // const [myPets, setMyPets] = useState<Tables<"pets">[]>([]);
   const closeModal = useModalStore((state) => state.closeModal);
 
-  const { data: myPets = [] } = useQuery({
+  const { data: myPets1 = [] } = useQuery({
     queryKey: ["pets"],
     enabled: !!currentUserId,
     queryFn: () => api.pets.getMyPets(currentUserId!),
   });
+
+  useEffect(() => {
+    (async () => {
+      const { data: profiles } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", currentUserId)
+        .single();
+
+      setProfiles(profiles);
+    })();
+  }, [currentUserId]);
+
+  // 대표 펫 선택하지 못하게 제외함
+  const myPets = myPets1?.filter((data) => data.id !== profiles?.firstPetId);
 
   const click = async (petId: number) => {
     const { data: pets } = await supabase
@@ -29,11 +47,14 @@ function ChooseMyPets({ setSelectedPetIds }: ChooseMyPetsProps) {
       .eq("id", petId)
       .single();
 
-    console.log(pets.id);
     if (!pets) return console.log("pets error");
     setSelectedPetIds((selectedPets) => [...selectedPets, pets.id]);
     closeModal();
   };
+
+  // useEffect(() => {
+  //   (async () => {})();
+  // }, [currentUserId]);
 
   return (
     <Modal>
