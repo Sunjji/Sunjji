f7e460a919689eb5ad4ce3671081a7c9f5923aaa
shin <sinhaeun1234@gmail.com>
2024-10-29 17:44:10 +0900

    feat: detailPage(미완성)

--- api/comments.api.ts
+++ api/comments.api.ts
@@ -3,7 +3,7 @@ import { supabase } from "@/supabase/client";
 async function getComments(diaryId: string) {
   const { data: comments, error } = await supabase
     .from("comments")
-    .select("*")
+    .select("*, author:profiles (*)")
     .eq("diaryId", Number(diaryId))
     .order("createdAt", { ascending: false }); // 최신 댓글이 위로 올라오게 하기
   return { comments, error };
