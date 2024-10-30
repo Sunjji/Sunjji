bd6e176b14976cce962b123d0d8fb533b92e2ce3
오성우 <ad19eq@naver.com>
2024-10-30 13:01:31 +0900

    feat: 대표펫 선택하는 버튼 추가(디자인필요)

---
+++ app/(providers)/(root)/(home)/_components/MyFirstPetSelectButton.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { supabase } from "@/supabase/client";
+import { useAuthStore } from "@/zustand/auth.store";
+import React, { useState } from "react";
+import { IoIosMore } from "react-icons/io";
+
+interface SelectButtonProps {
+  petId: number;
+}
+
+function MyFirstPetSelectButton({ petId }: SelectButtonProps) {
+  const [showSelectButton, setShowSelectButton] = useState(false);
+  const currentUserId = useAuthStore((state) => state.currentUserId);
+
+  const handleClickSelectButton = async () => {
+    const asr = await supabase
+      .from("profiles")
+      .update({ firstPetId: petId })
+      .eq("id", currentUserId!);
+
+    console.log(asr);
+
+    return alert("대표 반려동물을 정해다");
+  };
+
+  const handleClickSelect = () => {
+    setShowSelectButton(true);
+  };
+  return (
+    <>
+      <button>
+        <IoIosMore onClick={handleClickSelect} />
+      </button>
+      {showSelectButton && (
+        <div className="bg-white edit-button-container  top-[10px] right-[-55px]">
+          <button
+            className="border border-BrownPoint hover:bg-point rounded-md px-2 py-1"
+            onClick={handleClickSelectButton}
+          >
+            대표 반려동물 선택하기
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default MyFirstPetSelectButton;
