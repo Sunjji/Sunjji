9be0b4ef5a2f92f87ddf1b5b38fc23d59c0ba8d6
shin <sinhaeun1234@gmail.com>
2024-10-30 16:53:19 +0900

    style: scroll~

--- app/(providers)/(root)/(home)/_components/MyFirstPetSelectButton.tsx
+++ app/(providers)/(root)/(home)/_components/MyFirstPetSelectButton.tsx
@@ -2,7 +2,7 @@
 
 import { supabase } from "@/supabase/client";
 import { useAuthStore } from "@/zustand/auth.store";
-import React, { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IoIosMore } from "react-icons/io";
 
 interface SelectButtonProps {
@@ -24,20 +24,39 @@ function MyFirstPetSelectButton({ petId, onSelect }: SelectButtonProps) {
 
     onSelect(petId);
 
-    return alert("대표 반려동물을 정해다");
+    return alert("대표 반려동물을 정했다");
   };
 
   const handleClickSelect = () => {
     setShowSelectButton(true);
   };
 
+  // 수정, 삭제 버튼이 아닌 곳을 클릭했을 때 수정, 삭제 버튼 사라지게 만들기
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (
+        showSelectButton &&
+        !target.closest(".edit-button-container") &&
+        !target.closest(".edit-button")
+      ) {
+        setShowSelectButton(false);
+      }
+    },
+    [showSelectButton]
+  );
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   return (
     <>
-      <button>
-        <IoIosMore onClick={handleClickSelect} />
-      </button>
-      {showSelectButton && (
-        <div className="bg-white edit-button-container  top-[10px] right-[-55px]">
+      {showSelectButton ? (
+        <div className="bg-white edit-button-container">
           <button
             className="border border-BrownPoint hover:bg-point rounded-md px-2 py-1"
             onClick={handleClickSelectButton}
@@ -45,6 +64,10 @@ function MyFirstPetSelectButton({ petId, onSelect }: SelectButtonProps) {
             대표 반려동물 선택하기
           </button>
         </div>
+      ) : (
+        <button>
+          <IoIosMore onClick={handleClickSelect} />
+        </button>
       )}
     </>
   );
